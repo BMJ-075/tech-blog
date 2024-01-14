c340a6d0af96c340813f8a497c5247fe4d3e1dbc
Bhavya <bhavyajain075@gmail.com>
2024-01-14 23:14:24 +0530

    created basic routes for the pages

---
+++ src/app/topic/[topic]/page.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const Topic = (): React.JSX.Element => {
+  console.log("Topicpage");
+  return (
+    <div>
+      <h1>Topic Page </h1>
+    </div>
+  );
+};
+
+export default Topic;
---
+++ src/app/topic/[topic]/post/[post]/page.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const Post = (): React.JSX.Element => {
+  console.log("homepage");
+  return (
+    <div>
+      <h1>Post Page </h1>
+    </div>
+  );
+};
+
+export default Post;
