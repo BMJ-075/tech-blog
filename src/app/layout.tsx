b2fd34397564f84b2d366f264531c7e8ba812bd0
Bhavya <bhavyajain075@gmail.com>
2024-01-15 22:28:33 +0530

    addded tailwind config ,postcss , next config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/global.css";
 
 export const metadata = {
   title: "Next.js",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React, { type ReactElement } from "react";
+import "../styles/global.css";
 
 export default function Page(): ReactElement {
   return <h1>Hello, Next.js!</h1>;
