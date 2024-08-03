86d77e021d30b9222e54926d0b2c7f81e28558a8
bhavya <bhavyajain075@gmail.com>
2024-08-03 22:46:05 +0530

    added new navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,52 +1,75 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Github, BriefcaseBusiness } from "lucide-react";
 
 const NavBar = (): React.JSX.Element => {
   return (
-    <nav className="stick top-0 z-10 backdrop-blur-sm border-b-2 border-gray-300">
-      <div className="flex justify-between px-4 py-2">
-        <div id="nav-left" className=" flex items-center text-gray-800">
-          <div id="logo" className="px-2">
-            <Link href={"/"}> BMJ</Link>
+    <nav className="bg-white dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="font-edu self-center text-2xl font-bold whitespace-nowrap dark:text-white">
+            The Dev Den
+          </span>
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex items-center lg:order-2">
+            <Button size="sm" variant={"ghost"} className="px-1">
+              <span className="sr-only">Github</span>
+              <Github className="size-5" />{" "}
+            </Button>
+            <span className="mx-2 h-5 w-px bg-gray-200 dark:bg-gray-600 lg:inline"></span>
+            <Button size="sm" variant={"ghost"} className="px-1">
+              <span className="sr-only">My Website</span>
+              <BriefcaseBusiness className="size-5" />{" "}
+            </Button>
+            <span className="mx-2 h-5 w-px bg-gray-200 dark:bg-gray-600 lg:inline"></span>
+            {/* TODO - this is for toggling theme */}
+            {/* <Moon className="size-5" />
+            <Sun className="size-5" />{" "} */}
+            {/* <span className="mx-2 h-5 w-px bg-gray-200 dark:bg-gray-600 lg:inline"></span> */}
           </div>
-          <ul className="flex px-2 items-center">
-            <li className="px-2 hover:scale-105 hover:text-gray-900  font-medium transform transition-all ease-in">
-              <Link href={"/topic/React"}>React</Link>
-            </li>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href={"/topic/typescript"}
-                className="px-2 hover:scale-105 hover:text-gray-900  font-medium transform transition-all ease-in"
+                href="/topic/typescript"
+                className="block py-2 px-3 text-gray-900 bg-blue-700 rounded md:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500"
+                aria-current="page"
               >
                 Typescript
               </Link>
             </li>
             <li>
               <Link
-                href={"/topic/Node"}
-                className="px-2 hover:scale-105 hover:text-gray-900  font-medium transform transition-all ease-in"
+                href="/topic/react"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Node
+                React
               </Link>
             </li>
-          </ul>
-        </div>
-        <div id="nav-right" className="text-gray-300">
-          <ul className="flex">
-            <li className="h-7 w-7 m-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-              </svg>
+            <li>
+              <Link
+                href="/topic/nextjs"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Next Js
+              </Link>
             </li>
-            <li className="h-7 w-7 m-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z" />
-              </svg>
-            </li>
-            <li className="h-5 w-5 m-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                <path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" />
-              </svg>
+            <li>
+              <Link
+                href="/topic/doodle"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Doodle Dabble
+              </Link>
             </li>
           </ul>
         </div>
